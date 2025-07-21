a5f37cb35de9a4e59f0f7202e07f746da56bab3f
Shokhrukh <shoxrux@pro-data.tech>
2025-07-21 09:50:10 +0500

    Refactored

--- src/main/java/com/devcamp/socials/dto/UserResponse.java
+++ src/main/java/com/devcamp/socials/dto/UserResponse.java
@@ -3,9 +3,4 @@ package com.devcamp.socials.dto;
 import java.time.LocalDate;
 
 public record UserResponse(
-    Long id,
-    String firstName,
-    String lastName,
-    String username,
-    LocalDate birthDate,
-    boolean enabled) {}
+    Long id, String firstName, String lastName, String username, LocalDate birthDate) {}
