d47587fa7fa26fffba042b354f0323aa8ae017c8
Shokhrukh <shoxrux@pro-data.tech>
2025-08-27 12:33:35 +0500

    Refactored `UserProfileResponse` to use Java `record` for cleaner and more concise data representation.

--- src/main/java/com/devcamp/socials/dto/UserProfileResponse.java
+++ src/main/java/com/devcamp/socials/dto/UserProfileResponse.java
@@ -2,14 +2,9 @@ package com.devcamp.socials.dto;
 
 import java.time.LocalDate;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class UserProfileResponse {
-  private Long id;
-  private String firstName;
-  private String lastName;
-  private String username;
-  private LocalDate birthDate;
+public record UserProfileResponse(Long id, String firstName, String lastName, String username,
+                                  LocalDate birthDate) {
+
 }
